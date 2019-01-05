1862b1a71bf689724b3a8521a4c9aac075a2debe
SrtaCamelo <raissa_camelo@hotmail.com>
2019-01-05 14:15:56 -0300

    Adiciona alteracoes

--- Classification/MLP.py
+++ Classification/MLP.py
@@ -2,10 +2,25 @@
 import keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
+import pandas as pd
+import cv2
+
+def prepareDataSet(datapath):
+    #Shuffle
+    data = pd.read_csv(datapath)
+    data = data.sample(frac=1)
+    X = data.iloc[:,1:6]
+    Y = data['Class']
+    Y = pd.get_dummies(Y)
+    x_train, x_test = X.iloc[1:800], X.iloc[801:1071]
+    y_train, y_test = Y.iloc[1:800], Y.iloc[801:1071]
+
+    return x_train, x_test,y_train, y_test
+
 
 def mlp(x_train, x_test,y_train, y_test):
     neuralNetwork(x_train, x_test,y_train, y_test)
-    evaluation(x_test,y_test)
+    #evaluation(x_test,y_test)
 
 def neuralNetwork(x_train, x_test,y_train, y_test):
     model = Sequential()
@@ -20,6 +35,11 @@ def neuralNetwork(x_train, x_test,y_train, y_test):
     history = model.fit(x_train, y_train, batch_size=10, epochs=100)
 
 
-def evaluation(x_test,y_test):
+def evaluation(x_test,y_test, model):
     loss, accuracy = model.evaluate(x_test, y_test)
+    print(accuracy)
     #Insert other metrics soon
+#----Main---
+datapath = ""
+x_train, x_test,y_train, y_test = prepareDataSet(datapath)
+mlp(x_train, x_test,y_train, y_test)
